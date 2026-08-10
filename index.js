5006723f3508fa330f42d3f48c96819ce82b0f7a
Rebekah Alderfer <rebekah@titlesafe.tv>
2026-08-10 07:36:53 -0500

    added javascript for contact page

---
+++ index.js
@@ -0,0 +1,25 @@
+// template_q8qljhh
+// service_721w6az
+// PZxziNt2soWRWIOZP
+
+function contact() {
+  event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overaly--visible"
+  emailjs 
+    .sendForm(
+      'service_721w6az',
+      'template_q8qljhh',
+      event.target,
+      'PZxziNt2soWRWIOZP'
+    ).then(() => {
+      loading.classList.remove("modal__overlay--visible")
+      success.classList += " modal__overlay--visible"
+    }).catch(() => {
+      loading.classList.remove("modal__overlay--visible")
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at rebekah@titlesafe.tv."
+      )
+    })
+}
