56f0622036b4234655eb5bdbc5ad9076ed8e687b
Rebekah Alderfer <rebekah@titlesafe.tv>
2026-08-13 04:12:27 -0500

    added moving shappes to all 3 files; changed .shape position to absolute instead of fixed and added position relative to landing page to keep shapes at the top only

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
 // PZxziNt2soWRWIOZP
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor
+  const y = event.clientY * scaleFactor
+
+  for(let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0
+    const boolInteger = isOdd ? -1 : 1
+    shapes[i].style.transform = `translate(${x * boolInteger}px, ${y * boolInteger}px)`
+  }
+}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle
